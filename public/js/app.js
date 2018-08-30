491428fc34f5ea2bcb03a48071c727e37df6a971
BellaBe <bella.belgarokova@hotmail.com>
2018-08-30 14:10:59 +0200

    add working project

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,10 @@ window.addEventListener('load', () => {
     const ratesTemplate = Handlebars.compile($('#rates-template').html());
     const exchangeTemplate = Handlebars.compile($('#exchange-template').html());
     const historicalTemplate = Handlebars.compile($('#historical-template').html());
-    
+
     const router = new Router({
         mode: 'history',
-        page404:(path)=>{
+        page404: (path) => {
             const html = errorTemplate({
                 color: 'yellow',
                 title: 'Error 404 - page not found',
@@ -17,24 +17,128 @@ window.addEventListener('load', () => {
             el.html(html);
         }
     });
-    router.add('/', ()=>{
+    const api = axios.create({
+        baseURL: 'http://localhost:3000/api',
+        timeout: 5000
+    });
+    const showError = (error)=>{
+        const {title, message} =  error.response.data;
+        const html = errorTemplate({color: 'red', title, message});
+        el.html(html)
+    };
+
+    router.add('/', async()=>{
         let html = ratesTemplate();
-        el.html(html)
+        el.html(html);
+        try {
+            const response = await api.get('/rates');
+            const { base, date, rates } = response.data;
+            html = ratesTemplate({base, date, rates})
+            el.html(html)
+        } catch (error) {
+            showError(error)
+        } finally {
+            $('.loading').removeClass('loading')
+        }
     });
-    router.add('/exchange', ()=>{
+
+    const getConversionResults = async()=>{
+        const from = $('#from').val();
+        const to = $('#to').val();
+        const amount = $('#amount').val();
+        try {
+            const response = await api.post('/convert', { from, to });
+            const { rate } = response.data;
+            const result = rate * amount;
+            $('#result').html(`${to} ${result}`);
+        } catch (error) {
+            showError(error)
+        } finally {
+            $('#result-segment').removeClass('loading')
+        }
+    }
+
+    const convertRatesHandler = () =>{
+        if($('.ui.form').form('is valid')){
+            $('.ui.error.message').hide();
+            $('#result-segment').addClass('loading');
+            getConversionResults();
+            return false
+        }
+        return true
+    }
+
+    router.add('/exchange', async () => {
         let html = exchangeTemplate();
-        el.html(html)
+        el.html(html);
+        try {
+            const response = await api.get('/symbols');
+            const { symbols } = response.data;
+            html = exchangeTemplate({ symbols });
+            el.html(html);
+            $('.loading').removeClass('loading');
+            $('.ui.form').form({
+                fields: {
+                    from: 'empty',
+                    to: 'empty',
+                    amount: 'decimal'
+                }
+            });
+            $('.submit').click(convertRatesHandler);
+
+        } catch (error) {
+            showError(error)
+        }
     });
-    router.add('/', ()=>{
-        let html = historicalTemplate();
-        el.html(html)
+
+const getHistoricalRates = async ()=>{
+    const date = $('#date').val();
+    try {
+        const response = await api.post('/historical', { date });
+        const { base, rates } = response.data;
+        const html = ratesTemplate({ base, date, rates});
+        $('#historical-table').html(html);
+    } catch (error) {
+        showError(error)
+    } finally {
+        $('.segment').removeClass('loading')
+    }
+}
+
+const historicalRatesHandler = ()=>{
+    if($('.ui.form').form('is valid')){
+        $('.ui.error.message').hide();
+
+        $('.segment').addClass('loading');
+        getHistoricalRates();
+        return false;
+    }
+    return true
+}
+
+    router.add('/historical', () => {
+        const html = historicalTemplate();
+        el.html(html);
+        
+        $('#calendar').calendar({
+            type: 'date',
+            formatter: {
+                date: date => new Date(date).toISOString().split('T')[0]
+            }
+        })
+        $('.ui.form').form({
+            fields: {
+                date: 'empty'
+            }
+        })
+        $('.submit').click(historicalRatesHandler)
     });
 
     router.navigateTo(window.location.pathname);
 
     const link = $(`a[href$='${window.location.pathname}']`);
     link.addClass('active');
-    $('a').on('click', (e) =>{
+    $('a').on('click', (e) => {
         e.preventDefault();
 
         const target = $(e.target);
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-const { getRates } = require('./lib/fixer-service')
+const bodyParser = require('body-parser');
+const { getRates, getSymbols, getHistoricalRate } = require('./lib/fixer-service');
+const { convertCurrency } = require('./lib/free-currency-service');
 
 const app = express();
 
@@ -10,13 +12,87 @@ app.use(express.static('public'));
 
 app.use('/scripts', express.static(`${__dirname}/node_modules`));
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+const errorHandler = (err, req, res) =>{
+    if(err.response){
+        res.status(403).send({
+            title:'Server responded with an error', message: err.message
+        })
+    }else if(err.request){
+        res.status(503).send({
+            title: 'Unable to comunicate with server', message: err.message
+        })
+    }else{
+        res.status(500).send({
+            title: 'An unexpected error occured', message: err.message
+        })
+    }
+}
+
+app.get('/api/rates', async (req, res) =>{
+    try {
+        const data = await getRates();
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data)
+    } catch (err) {
+        errorHandler(error, req, res)
+    }
+});
+app.get('/api/symbols', async(req, res) =>{
+    try {
+        const data  = await getSymbols();
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data)
+    } catch (error) {
+        errorHandler(error, req, res)
+    }
+});
+app.post('/api/convert', async (req, res) =>{
+    try {
+        const { from, to } = req.body;
+        const data =  await convertCurrency(from, to);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data)
+    } catch (error) {
+       errorHandler(error, req, res) 
+    }
+});
+app.post('/api/historical', async(req, res) =>{
+    try {
+        const { date } = req.body;
+        const data = await getHistoricalRate(date);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data)
+    } catch (error) {
+        errorHandler(error, req, res)
+    }
+})
+
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
+// const test = async() => {
+//     const data = await getRates();
+//     console.log(data)
+// }
+// const test = async() => {
+//     const data = await getSymbols();
+//     console.log(data)
+// }
+// const test = async() => {
+//     const data = await convertCurrency('USD', 'KES');
+//     console.log(data)
+// }
 const test = async() => {
-    const data = await getRates();
+    const data = await getHistoricalRate('2017-07-14');
     console.log(data)
 }
 test();
+
 app.listen(port, ()=> {
     console.log(`Listening on port ${port}`);
 })
